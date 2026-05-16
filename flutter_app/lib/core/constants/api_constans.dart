ed789546bc2178328aa1a7fe721ddc8f16db6b59
irpan <irfanmaruf0000@gmail.com>
2026-05-16 17:43:35 +0700

    feat: implement student internship application controller and configure API constants for Flutter app

--- flutter_app/lib/core/constants/api_constans.dart
+++ flutter_app/lib/core/constants/api_constans.dart
@@ -1,8 +1,9 @@
 class ApiConstants {
   // Ganti IP dengan IP komputer kamu (jangan localhost jika pakai emulator/HP fisik)
-  static const String baseUrl = "http://127.0.0.1:8000/api"; //untuk web
+  // static const String baseUrl = "http://127.0.0.1:8000/api"; //untuk web
 
   // static const String baseUrl = "http://10.0.2.2:8000/api"; // Untuk Emulator
 
-  // static const String baseUrl = "http://192.168.1.8:8000/api"; //untuk hp fisik
+  static const String baseUrl =
+      "http://10.74.255.224:8000/api"; //untuk hp fisik
 }
